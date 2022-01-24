ac0e2a3de0431682752390b0cada93e2dfb069b4
83544844@qq.com <83544844@qq.com>
2022-01-24 15:34:29 +0800

    hahad

--- src/main/scala/cn/doitedu/sparksql/C04_各类数据源加载成dataset.scala
+++ src/main/scala/cn/doitedu/sparksql/C04_各类数据源加载成dataset.scala
@@ -1,6 +1,6 @@
 package cn.doitedu.sparksql
 
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SaveMode, SparkSession}
 import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 
 import java.util.Properties
@@ -15,8 +15,11 @@ object C04_各类数据源加载成dataset {
     // loadCsv(spark)
     // loadJson(spark)
     // loadJson2(spark)
-    loadJDBC(spark)
+    // loadJDBC(spark)
 
+    df写出成各种数据源(spark)
+    // loadParquet(spark)
+    // loadOrc(spark)
 
     spark.close()
   }
@@ -50,24 +53,30 @@ object C04_各类数据源加载成dataset {
 
     // {"id":1,"name":"aa","scores":[95,80,86,87],"age":18,"gender":"male"}
     val schema = StructType(Seq(
+      // 定义基本类型字段
       StructField("id", DataTypes.IntegerType),
       StructField("name", DataTypes.StringType),
+      // 定义struct类型字段
       StructField("school",
         DataTypes.createStructType(Array(
           StructField("name", DataTypes.StringType),
           StructField("graduate", DataTypes.StringType),
           StructField("rank", DataTypes.IntegerType)))
       ),
+      // 定义Map类型字段
       StructField("info", DataTypes.createMapType(DataTypes.StringType, DataTypes.StringType)), // info字段，因为每一行中的内部属性不同，所以用HashMap类型比较合适
+      // 定义Array类型字段
       StructField("scores", DataTypes.createArrayType(DataTypes.IntegerType)), // 数组类型
       StructField("age", DataTypes.DoubleType),
       StructField("gender", DataTypes.StringType)
     ))
 
-    // val df = spark.read.json("data/sql/stu2.txt")   // 自动推断的话，info字段会被推断成 struct类型
-    val df = spark.read.schema(schema).json("data/sql/stu2.txt")
+    // val df = spark.read.json("data/sql/stu2.json")   // 自动推断的话，info字段会被推断成 struct类型
+    val df = spark.read.schema(schema).json("data/sql/stu2.json")
     df.printSchema()
     df.show(false)
+
+    spark.read.parquet()
 
 
     // TODO  计算每所学校毕业生的平均年龄
@@ -134,4 +143,56 @@ object C04_各类数据源加载成dataset {
 
   }
 
+  // 读parquet数据源
+  // parquet数据源不用手动传schema，因为parquet是自我描述型的列式存储文件格式（文件内部就带有表结构信息）
+  def loadParquet(spark:SparkSession):Unit = {
+    val df = spark.read.parquet("data/sql/out-parquet")
+    df.printSchema()
+    df.show(false)
+  }
+
+  // 读orc数据源
+  // orc文件源不用手动传schema，因为 orc 是自我描述型的列式存储文件格式（文件内部就带有表结构信息）
+  def loadOrc(spark:SparkSession):Unit = {
+    val df = spark.read.orc("data/sql/out-orc")
+    df.printSchema()
+    df.show(false)
+  }
+
+
+  def df写出成各种数据源(spark:SparkSession):Unit = {
+    val schema = StructType(Seq(
+      StructField("id",DataTypes.IntegerType),
+      StructField("name",DataTypes.StringType),
+      StructField("age",DataTypes.IntegerType),
+      StructField("gender",DataTypes.StringType),
+      StructField("info",DataTypes.createMapType(DataTypes.StringType,DataTypes.StringType)),
+      StructField("scores",DataTypes.createArrayType(DataTypes.IntegerType)),
+      StructField("school",DataTypes.createStructType(Array(StructField("name",DataTypes.StringType),StructField("graduate",DataTypes.StringType),StructField("rank",DataTypes.IntegerType))))
+    ))
+
+    val df: DataFrame = spark.read.schema(schema).json("data/sql/stu2.json")
+
+    // 将df写成一个parquet文件
+    df.write.parquet("data/sql/out-parquet/")
+    df.write.orc("data/sql/out-orc/")
+    df.write.json("data/sql/out-json/")
+    // df.write.csv("data/sql/out-csv/")  简单结构的dataframe才能写成csv文件 ,不支持array、map、struct复合类型
+    val df2 = df.drop("info").drop("scores").drop("school") // 丢弃一个列
+    df2.write.csv("data/sql/out-csv/")
+
+    // 将df写成mysql的表
+    val props = new Properties()
+    props.setProperty("user","root")
+    props.setProperty("password","123456")
+
+    val df3 = df.drop("scores", "school", "info")  // 因为jdbc中不存在array、map、struct类型
+    //df2.write.jdbc("jdbc:mysql://localhost:3306/abc","t_x",props)  // 如果表不存在，会自动建表
+
+    // SaveMode有多种取值:
+    // overWrite ==> 覆盖写入；  append ==> 追加写入 ;
+    // Ignore ==> 如果目标表已存在则啥也不做  ErrorIfExists ==> 如果目标表已存在，就抛个异常给你
+    df3.write.mode(SaveMode.Overwrite).jdbc("jdbc:mysql://localhost:3306/abc","t_x",props)
+
+  }
 }
---
+++ src/main/scala/cn/doitedu/sparksql/X01_随堂练习_1.scala
@@ -0,0 +1,20 @@
+package cn.doitedu.sparksql
+
+object X01_随堂练习_1 {
+  def main(args: Array[String]): Unit = {
+
+    // 把mysql中的一个表映射成sparkSql的一个dataframe
+
+
+
+
+    // 然后在sparkSql中写sql来进行查询(查询到所有的3级、4级行政单位地点）得到如下结果：
+    //      江苏省,盐城市,大丰区,120.58506449026754,33.26590852607762
+
+
+    // 然后将结果写成一个parquet文件，并也写入mysql
+
+
+
+  }
+}
