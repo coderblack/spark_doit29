7c1c13f75af86b3d908c21d4b249e73526d459e2
83544844@qq.com <83544844@qq.com>
2022-02-10 12:04:37 +0800

    hahad

--- src/main/scala/cn/doitedu/exersise/X01_部门预算.scala
+++ src/main/scala/cn/doitedu/exersise/X01_部门预算.scala
@@ -1,5 +1,8 @@
 package cn.doitedu.exersise
 
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
+
 /**
  * 求，预算最低的3个部门的： 部门id，部门预算，部门人数
  *    预算最高的3个部门的： 部门id，部门预算，部门员工总薪资
@@ -7,5 +10,105 @@ package cn.doitedu.exersise
  */
 object X01_部门预算 {
 
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("")
+      .master("local")
+      .getOrCreate()
+
+
+    val schema1 = StructType(Seq(
+      StructField("departmentid",DataTypes.IntegerType),
+      StructField("yusuanAmount",DataTypes.IntegerType)
+    ))
+
+    //departmentid,employeeid,salary
+    val schema2 = StructType(Seq(
+      StructField("departmentid",DataTypes.IntegerType),
+      StructField("employeeid",DataTypes.IntegerType),
+      StructField("salary",DataTypes.IntegerType)
+    ))
+
+
+    val yuSuan = spark.read.schema(schema1).option("header", "true").csv("data/yusuan/yusuan.txt")
+    val department = spark.read.schema(schema2).option("header", "true").csv("data/yusuan/department.txt")
+
+    yuSuan.createTempView("yusuan")
+    department.createTempView("department")
+
+    val res = spark.sql(
+      """
+        |select
+        |  o1.departmentid,
+        |  o1.yusuanAmount,
+        |  o2.cnt
+        |
+        |from
+        |(
+        |   select
+        |     departmentid,yusuanAmount
+        |   from yusuan
+        |   order by yusuanAmount
+        |   limit 3
+        |) o1
+        |
+        |join
+        |
+        |(
+        |   select
+        |     departmentid,
+        |     count(1) as cnt
+        |   from department
+        |   group by departmentid
+        |) o2
+        |
+        |on o1.departmentid=o2.departmentid
+        |
+        |""".stripMargin)
+
+    res.show(100,false)
+
+
+    /**
+     * 求每个部门薪资最高的3个员工，以及该部门的今年的预算金额
+     */
+    val res2 = spark.sql(
+      """
+        |select
+        |     o2.departmentid,
+        |     o2.employeeid,
+        |     o2.salary,
+        |     yusuan.yusuanAmount
+        |from
+        |(
+        |
+        |   select
+        |     departmentid,
+        |     employeeid,
+        |     salary
+        |   from
+        |   (
+        |      select
+        |         departmentid,
+        |         employeeid,
+        |         salary,
+        |         row_number() over(partition by departmentid order by salary desc) as rn
+        |      from department
+        |   ) o
+        |   where rn<=3
+        |) o2
+        |join
+        |yusuan
+        |on o2.departmentid=yusuan.departmentid
+        |
+        |""".stripMargin)
+    res2.show(100,false)
+
+    spark.close()
+
+
+
+  }
 
 }
--- src/main/scala/cn/doitedu/exersise/X02_最大在线人数.scala
+++ src/main/scala/cn/doitedu/exersise/X02_最大在线人数.scala
@@ -1,8 +1,111 @@
 package cn.doitedu.exersise
 
+import org.apache.spark.sql.SparkSession
+
 /**
- * 针对 登录登出记录，求最大在线人数
+ * 针对登录登出记录，求最大在线人数
  */
 object X02_最大在线人数 {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("")
+      .master("local")
+      .config("spark.sql.shuffle.partitions","1")  // spark.default.parallelism
+      .getOrCreate()
+
+
+    // uid,login_time,logout_time
+    val df = spark.read.option("header", "true").option("inferSchema", "true").csv("data/maxonline/")
+    df.createTempView("df")
+
+    val range2Array  = (s:Int, e:Int)=>{
+      s.until(e).toArray
+    }
+    spark.udf.register("range2arr",range2Array)
+
+
+    /**
+     * 先把每个用户的在线时间区间炸裂成多条
+     * u1,1
+       u1,2
+       u2,1
+       u2,2
+       u2,3
+       u3,2
+       u4,3
+       u5,2
+       u5,3
+       u5,4
+       u6,3
+       u6,4
+       u6,5
+     然后再统计每一个时间点上的人数
+     弊端：数据量急剧膨胀，效率极低！
+     */
+
+    spark.sql(
+      """
+        |select
+        |  max(cnt) as max_online
+        |from (
+        |    select
+        |      online_time,count(1) as cnt
+        |    from
+        |    (
+        |       select
+        |         uid,
+        |         online_time
+        |       from df lateral view explode(range2arr(login_time,logout_time)) o as online_time
+        |    ) o
+        |    group by online_time
+        |) o
+        |
+        |
+        |""".stripMargin).show(100,false)
+
+
+    /**
+     * 先把每个用户的登入登出记录变成2条记录，并且打上登入，还是登出的区分标记（1，或-1）
+     * u1,1,1
+       u1,3,-1
+       u2,1,1
+       u2,4,-1
+       u3,2,1
+       u3,3,-1
+       u4,3,1
+       u4,4,-1
+       u5,2,1
+       u5,5,-1
+       u6,3,1
+       u6,6,-1
+
+       -- 时间排序 ，然后sum over 标记字段
+       u1,1,1   ==> 1
+       u2,1,1   ==> 2
+       u3,2,1   ==> 3
+       u5,2,1   ==> 4
+       u1,3,-1  ==> 3
+       u3,3,-1  ==> 2
+       u4,3,1   ==> 3
+       u6,3,1   ==> 4
+       u2,4,-1  ==> 3
+       u4,4,-1  ==> 2
+       u5,5,-1  ==> 1
+       u6,6,-1  ==> 0
+
+       -- sum over时有一个小细节，观察下面的特殊情况，不能用最前加到当前行！
+       sum(flag) over(order by time)
+       u3,1,1    ==> 1
+       u1,2,1    ==> 0
+       u2,2,-1   ==> 0
+       u3,2,-1   ==> 0
+     */
+
+
+    spark.close()
+
+
+  }
 
 }
